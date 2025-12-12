b73f9ed33fdb96ff6d93e416e28e8b449f130df9
Devin Garcia <devingarcia5423@gmail.com>
2025-12-12 17:17:21 -0500

    locally host images and remove remote patterns from next config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,22 @@ import HorizontalRule from './components/horizontal-rule';
 import Articles from './components/articles';
 import Contributions from './components/contributions';
 import About from './components/about';
-import Image from 'next/image';
 
 export default function Home() {
 	const photos: string[] = [
-		'https://res.cloudinary.com/dmmn0gqaf/image/upload/v1764958394/stella-raine-chu/stella-raine-chu-1_fvrnb5.jpg',
-		'https://res.cloudinary.com/dmmn0gqaf/image/upload/v1764958394/stella-raine-chu/stella-raine-chu-2_bt7tja.jpg',
-		'https://res.cloudinary.com/dmmn0gqaf/image/upload/v1764958395/stella-raine-chu/stella-raine-chu-3_mlksqt.jpg',
-		// 'https://res.cloudinary.com/dmmn0gqaf/image/upload/v1764958396/stella-raine-chu/stella-raine-chu-4_bqgioi.jpg',
+		'zzelgf0qovwuyyhtjebu.webp',
+		'l5ebqdpms3x30wvixmov.webp',
+		'd0c8qvzhqeijgcwrlkvq.webp',
 	];
 
-	let randPhotoIndex = Math.floor((photos.length * 3 * Math.random()) / 3);
+	let randPhotoIndex = Math.floor(Math.random() * (photos.length - 1 + 1));
 
 	return (
 		<>
 			<Header />
 			<main className={styles.main}>
 				<div className={styles.cover}>
-					<Image
-						src={photos[randPhotoIndex]}
-						alt='Photo of Stella Raine Chu'
-						width={200}
-						height={200}
-						unoptimized
-					/>
+					<img src={photos[randPhotoIndex]} alt='Photo of Stella Raine Chu' />
 					<HorizontalRule width='100%' />
 					<About />
 				</div>
