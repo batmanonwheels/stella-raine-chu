80bc87413560e993774397603529a180e9b7f812
Devin Garcia <devingarcia5423@gmail.com>
2026-01-05 14:48:10 -0500

    add photo type to sanity, and fetching logic to component

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { articleType } from '@/app/studio/schemaTypes/articleType';
+import { photoType } from '@/app/studio/schemaTypes/photoType';
 import { type SchemaTypeDefinition } from 'sanity';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [articleType],
+	types: [articleType, photoType],
 };
